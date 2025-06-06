d85a508ad70a0736735f21bf8c795c67a5d191b3
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-06 11:06:13 -0400

    Added code for accepting and processing command-line arguments

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # minigrep_rust
+
+This project aims to create a simpler version of the classic Linux command-line tool *grep*. 
+
+The project was written in Rust.
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+C:\Users\nadir\Documents\code_files\rust\minigrep_rust\target\debug\minigrep.exe: C:\Users\nadir\Documents\code_files\rust\minigrep_rust\src\main.rs
